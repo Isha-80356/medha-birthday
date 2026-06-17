e171032b8ba87486c7ac263d42c1512545fdfa7f
Monisha Sharma <24bcs80356@cuchd.in>
2026-06-17 23:17:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function openGift(){
+
+document.getElementById("giftPage").style.display="none";
+
+document.getElementById("mainPage").style.display="block";
+
+document.getElementById("music").play();
+
+}
+
+
+
+function showPhotos(){
+
+document.getElementById("photos").style.display="block";
+
+}
+
+
+
+function openLetter(){
+
+document.getElementById("letter").style.display="block";
+
+}
